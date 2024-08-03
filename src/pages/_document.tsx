607feb18901971b890f4a90668d5b1eabc294021
Saptarshi Basu <123saptarshi.basu@gmail.com>
2024-08-03 15:21:51 +0530

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>ETHAntwerp</title>
+        <link
+          href="https://fonts.cdnfonts.com/css/against-2"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
